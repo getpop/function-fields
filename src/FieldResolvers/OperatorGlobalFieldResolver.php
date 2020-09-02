313f8d10739b561185b33022199e27d58f5c2455
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:45:38 +0800

    Added object type to resultItem for resolveValue

--- src/FieldResolvers/OperatorGlobalFieldResolver.php
+++ src/FieldResolvers/OperatorGlobalFieldResolver.php
@@ -408,8 +408,22 @@ class OperatorGlobalFieldResolver extends AbstractGlobalFieldResolver
         return null;
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         switch ($fieldName) {
             case 'sprintf':
                 return sprintf($fieldArgs['string'], ...$fieldArgs['values']);
